f72c02dc0b29dd4cd2665ab6a2a9ca2dc3699f98
Avi Vahl <avi.vahl@wix.com>
2018-12-21 14:51:39 +0200

    node: upgrade to node 10 and above

--- packages/node/src/node-fs.ts
+++ packages/node/src/node-fs.ts
@@ -1,12 +1,15 @@
 import path from 'path'
 import {
-    lstat, lstatSync, mkdir, mkdirSync, readdir, readdirSync, readFile, readFileSync, realpath, realpathSync,
-    rename, renameSync, rmdir, rmdirSync, stat, statSync, unlink, unlinkSync, writeFile, writeFileSync, isCaseSensitive,
-    copyFile, copyFileSync
-} from 'proper-fs'
+    existsSync, lstatSync, mkdirSync, readdirSync, readFileSync, realpathSync,
+    rmdirSync, statSync, unlinkSync, writeFileSync, copyFileSync, renameSync, promises
+} from 'fs'
+const { lstat, mkdir, readdir, readFile, realpath, rmdir, stat, unlink, writeFile, copyFile, rename } = promises
+
 import { createAsyncFileSystem, createSyncFileSystem } from '@file-services/utils'
 import { IBaseFileSystem, IFileSystem } from '@file-services/types'
 import { NodeWatchService, INodeWatchServiceOptions } from './watch-service'
+
+const caseSensitive = !existsSync(__filename.toUpperCase())
 
 export interface ICreateNodeFsOptions {
     watchOptions?: INodeWatchServiceOptions
@@ -25,7 +28,7 @@ export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSyste
     return {
         path,
         watchService: new NodeWatchService(options && options.watchOptions),
-        caseSensitive: isCaseSensitive,
+        caseSensitive,
         copyFile,
         copyFileSync,
         lstat,
@@ -34,7 +37,7 @@ export function createBaseNodeFs(options?: ICreateNodeFsOptions): IBaseFileSyste
         mkdirSync,
         readdir,
         readdirSync,
-        readFile(filePath, encoding = 'utf8') { return readFile(filePath, encoding) },
+        readFile(filePath, encoding = 'utf8') { return readFile(filePath, encoding) as Promise<string> },
         readFileRaw(filePath) { return readFile(filePath) },
         readFileSync(filePath, encoding = 'utf8') { return readFileSync(filePath, encoding) },
         readFileRawSync(filePath) { return readFileSync(filePath) },
